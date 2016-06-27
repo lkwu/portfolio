6f7eb9854be6e68de0e3f440b198832c3da1fc90
lkwu <legray1109@gmail.com>
2016-06-27 19:52:26 +0800

    Setup the contact form

---
+++ app/models/contact.rb
@@ -0,0 +1,16 @@
+class Contact < MailForm::Base
+
+  attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message
+  attribute :nickname,  :captcha  => true
+
+  def headers
+    {
+      :subject => "My Contact Form",
+      :to => "lunkaiwu@gmail.com",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+
+end
