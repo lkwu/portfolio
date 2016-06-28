c5f9dc4fd624c8ce3741d2e5e78c12eb97a060a0
lkwu <legray1109@gmail.com>
2016-06-28 20:40:12 +0800

    Added prev and next post link in show pages and modified the layout

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ActiveRecord::Base
 
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  def prev
+    Post.where(["id > ?", id ]).first
+  end
+
+  def next
+    Post.where(["id < ?", id]).last
+  end
+
 end
